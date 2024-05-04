c7ed8ba495889ddd3eb5cc1c4d73d48f11afcd11
badrmohamed123 <167132960+badrmohamed123@users.noreply.github.com>
2024-05-04 19:03:50 +0300

    Add project files.

---
+++ Clean Architecture.infrastructur/persistences/Data/ApplicationDb ConText.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture.infrastructur.persistences.Data
+{
+    public class ApplicationDb_ConText : DbContext
+    {
+        public ApplicationDb_ConText(DbContextOptions<ApplicationDb_ConText>options) : base (options)
+        {
+            
+        }
+    }
+}
---
+++ Clean Architecture/interfaces/IRebository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture.interfaces
+{
+    public interface IRebository<TEntity> : IRebository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> AddAsync(TEntity entity);
+        ValueTask<TEntity> Get(String EntityId);
+        ValueTask<IEnumerable<TEntity>> GetAll();
+        ValueTask<TEntity> UpdateAsync (TEntity entity);
+        ValueTask<TEntity> DeleteAsync (TEntity entity);
+    }
+}
